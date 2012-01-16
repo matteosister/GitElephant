fdeb054fb23b19de377a29fff9f36d296a9f3839
matteosister <matteog@gmail.com>
2012-01-16 15:32:29 +0100

    composer and package.ini requires for Process

--- src/GitElephant/Command/Caller.php
+++ src/GitElephant/Command/Caller.php
@@ -15,6 +15,7 @@
 namespace GitElephant\Command;
 
 use GitElephant\GitBinary;
+use Symfony\Component\Process\Process;
 
 /**
  * Caller
@@ -37,13 +38,6 @@ class Caller
      * @var string
      */
     private $repositoryPath;
-
-    /**
-     * error stream from the command
-     *
-     * @var string
-     */
-    private $stdErr;
 
     /**
      * the output lines of the command
@@ -91,50 +85,27 @@ class Caller
             $cmd = $this->binary->getPath() . ' ' . $cmd;
         }
 
-        $descriptorSpec = array(
-            0 => array("pipe", "r"), // Input
-            1 => array("pipe", "w"), // Output
-            2 => array("pipe", "w") // Error
-        );
-
-        $pipes   = array();
-        $process = proc_open(
-            $cmd,
-            $descriptorSpec,
-            $pipes,
-            $cwd == null ? $this->repositoryPath : $cwd,
-            null
-        );
-
-        if (is_resource($process)) {
-            fclose($pipes[0]);
-            while ($line = fgets($pipes[1])) {
-                if ($line !== false) {
-                    $this->outputLines[] = rtrim($line);
-                }
-            }
-            $this->stdErr = stream_get_contents($pipes[2]);
-            fclose($pipes[1]);
-            fclose($pipes[2]);
-            if ($this->getError() !== false) {
-                throw new \RuntimeException(sprintf('Cannot execute "%s", message: "%s", folder: "%s"', $cmd, $this->getError(), $this->repositoryPath));
-            }
-            return $this;
-        } else {
-            fclose($pipes[1]);
-            fclose($pipes[2]);
-            throw new \RuntimeException(sprintf('Cannot execute "%s"', $cmd));
+        $process = new Process($cmd, $cwd == null ? $this->repositoryPath : $cwd);
+        $process->setTimeout(15000);
+        $process->run();
+        if (!$process->isSuccessful()) {
+            throw new \RuntimeException($process->getErrorOutput());
         }
+        $this->outputLines = array_map('rtrim', explode(PHP_EOL, $process->getOutput()));
+        $this->outputLines = array_filter($this->outputLines, array($this, 'clearBlankLines'));
+        return $this;
     }
 
     /**
-     * returns the error output of the last executed command
+     * filter an array of output lines and remove the empty ones.
      *
-     * @return bool|string
+     * @param $var the array value
+     *
+     * @return bool
      */
-    public function getError()
+    private function clearBlankLines($var)
     {
-        return $this->stdErr == '' ? false : trim($this->stdErr);
+        return $var !== '';
     }
 
     /**
