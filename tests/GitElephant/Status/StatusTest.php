b9c3cf064be91cb7c0a649465e68e7324f29f880
Matteo <matteog@gmail.com>
2013-12-15 16:54:05 +0100

    fix for unstage in StatusTest

--- tests/GitElephant/Status/StatusTest.php
+++ tests/GitElephant/Status/StatusTest.php
@@ -127,12 +127,8 @@ class StatusTest extends TestCase
      */
     public function testWorkingTreeStatus()
     {
-        /*$this->markTestSkipped(
-            'Caller::execute throws a RuntimeException here because. Repository::unstage
-invokes "git reset HEAD -- test", which returns 1 (not 0) on git < 1.8, even though it executes successfully.
-On new git version this is not happening anymore.'
-        );*/
-
+        $this->addFile('initial file');
+        $this->getRepository()->commit('first commit', true);
         $this->addFile('test', null, 'test content');
         $wt = $this->repository->getWorkingTreeStatus();
         $this->assertCount(1, $wt->untracked());
