456381106121df307edda7dda0be93c8dbc8f471
Matteo <matteog@gmail.com>
2013-12-22 17:06:24 +0100

    method and tests

--- tests/GitElephant/Status/StatusTest.php
+++ tests/GitElephant/Status/StatusTest.php
@@ -127,7 +127,11 @@ class StatusTest extends TestCase
      */
     public function testWorkingTreeStatus()
     {
-        $this->markTestSkipped('See comments below');
+        /*$this->markTestSkipped(
+            'Caller::execute throws a RuntimeException here because. Repository::unstage
+invokes "git reset HEAD -- test", which returns 1 (not 0) on git < 1.8, even though it executes successfully.
+On new git version this is not happening anymore.'
+        );*/
 
         $this->addFile('test', null, 'test content');
         $wt = $this->repository->getWorkingTreeStatus();
