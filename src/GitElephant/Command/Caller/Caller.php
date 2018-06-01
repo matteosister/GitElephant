5247fd8b4402034f4bd2b24771019552645f2139
axel-rutz <github@aexl.de>
2018-06-01 13:42:35 +0200

    Fixed #124 getOutput() omits line breaks (#136)

--- src/GitElephant/Command/Caller/Caller.php
+++ src/GitElephant/Command/Caller/Caller.php
@@ -142,13 +142,13 @@ class Caller implements CallerInterface
     }
 
     /**
-     * returns the raw output of the last executed command
+     * returns the output of the last executed command
      *
      * @return string
      */
     public function getOutput()
     {
-        return implode(" ", $this->outputLines);
+        return implode("\n", $this->outputLines);
     }
 
     /**
