2af905ebffaa413f1c6bb18637d9bcd0bc96afad
Tim Bernhard <tim@bernhard-webstudio.ch>
2022-01-30 08:49:40 +0100

    Update GitHub action's setup-php due to CVE

--- ecs.php
+++ ecs.php
@@ -20,7 +20,10 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $parameters = $containerConfigurator->parameters();
 
-    $parameters->set(Option::SETS, [SetList::CLEAN_CODE, SetList::PSR_12]);
+    $parameters->set(Option::SETS, [
+        SetList::CLEAN_CODE,
+        SetList::PSR_12
+    ]);
 
     $parameters->set(Option::PATHS, [__DIR__ . '/src', __DIR__ . '/tests']);
 
