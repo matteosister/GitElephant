15379efb6bccf0ca90998490a7f8c4271d40c955
Tim Bernhard <tim@bernhard-webstudio.ch>
2022-06-30 08:17:01 +0200

    Upgrade ecs config

--- ecs.php
+++ ecs.php
@@ -6,26 +6,13 @@ use Symplify\CodingStandard\Fixer\LineLength\LineLengthFixer;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $services = $containerConfigurator->services();
-    // $services->set(ArraySyntaxFixer::class)->call('configure', [
-    //     'syntax' => 'short'
-    // ]);
+return static function (ECSConfig $configurator): void {
+    $configurator->paths(Option::PATHS, [__DIR__ . '/src', __DIR__ . '/tests']);
 
-    // $services->set(LineLengthFixer::class)->call('configure', [
-    //     "max_line_length" => 100,
-    //     "break_long_lines" => true, # default: true
-    //     "inline_short_lines" => false # default: true
-    // ]);
-
-    $parameters = $containerConfigurator->parameters();
-
-    $parameters->set(Option::SETS, [
+    $configurator->sets([
         SetList::CLEAN_CODE,
         SetList::PSR_12
     ]);
 
-    $parameters->set(Option::PATHS, [__DIR__ . '/src', __DIR__ . '/tests']);
-
-    $parameters->set(Option::SKIP, ['Unused variable $deleted.' => ['src/GitElephant/Objects/Diff/DiffChunk.php'], 'Unused variable $new.' => ['src/GitElephant/Objects/Diff/DiffChunk.php']]);
+    $configurator->skip(['Unused variable $deleted.' => ['src/GitElephant/Objects/Diff/DiffChunk.php'], 'Unused variable $new.' => ['src/GitElephant/Objects/Diff/DiffChunk.php']]);
 };
